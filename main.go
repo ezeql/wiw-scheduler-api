3e1988b206b00191c48527b81c03a4b5c183ff29
Ezequiel Moreno <ezeqle@gmail.com>
2015-10-06 19:39:15 -0300

    handle mysql connection errors

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/ezeql/wiw-scheduler-api/wiw"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
 	"strconv"
 )
@@ -13,13 +14,16 @@ type schedulerService struct {
 }
 
 func main() {
-
-	repository, _ := wiw.NewMySQLRepo("homestead:secret@tcp(localhost:33060)/wheniwork?charset=utf8&parseTime=True&loc=Local")
+	repository, err := wiw.NewMySQLRepo("homestead:secret@tcp(localhost:33060)/wheniwork?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		log.Fatal("ERROR: Cannot build repository")
+		log.Fatal(error)
+		return
+	}
 	router := gin.Default()
 	schedulerAPI := schedulerService{repository}
 
 	authMiddleware := router.Group("/", schedulerAPI.Authorization)
-
 	authMiddleware.POST("/shifts/", schedulerAPI.CreateShift)
 	authMiddleware.GET("/shifts/", schedulerAPI.ViewShiftsByDate)
 
@@ -32,7 +36,6 @@ func main() {
 	setIDMiddleware.GET("/users/:id", schedulerAPI.ViewUser)
 
 	router.Run(":3001")
-
 }
 
 func (ss *schedulerService) Authorization(c *gin.Context) {
@@ -57,7 +60,6 @@ func (ss *schedulerService) ViewShiftsForUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"shifts": shifts})
-
 }
 
 func (ss *schedulerService) ViewColleagues(c *gin.Context) {
@@ -89,7 +91,6 @@ func (ss *schedulerService) ViewManagers(c *gin.Context) {
 }
 
 func (ss *schedulerService) CreateShift(c *gin.Context) {
-
 }
 
 func (ss *schedulerService) UpdateShift(c *gin.Context) {
@@ -104,7 +105,6 @@ func (ss *schedulerService) ViewShiftsByDate(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": shifts})
-
 }
 
 func (ss *schedulerService) ViewUser(c *gin.Context) {
@@ -115,12 +115,10 @@ func (ss *schedulerService) ViewUser(c *gin.Context) {
 		return
 	}
 	c.JSON(http.StatusOK, gin.H{"data": user})
-
 }
 
 func (ss *schedulerService) handleError(c *gin.Context, err error) {
 	if err := err.Error(); err == "record not found" {
 		c.JSON(404, gin.H{"error": "record not found"})
 	}
-
 }
